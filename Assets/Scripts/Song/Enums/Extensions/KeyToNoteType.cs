d6ffdd5b725b45ebd03de58433b25e4b1935bf0b
jackie <photonicdog@gmail.com>
2021-09-01 15:06:04 +0100

    feat: Create extension class for converting KeyType to NoteType enum

---
+++ Assets/Scripts/Song/Enums/Extensions/KeyToNoteType.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.Song.Enums;
+using System;
+
+namespace Assets.Scripts.Song.Extensions
+{
+    public static class KeyToNoteType 
+    {
+        public static NoteType FromKeyType(this KeyType keyType)
+        {
+            switch (keyType)
+            {
+                case KeyType.L1:
+                case KeyType.R1:
+                    return NoteType.Blue;
+
+                case KeyType.L2:
+                case KeyType.R2:
+                    return NoteType.Pink;
+
+                case KeyType.L3:
+                case KeyType.R3:
+                    return NoteType.Green;
+
+                case KeyType.Bar:
+                    return NoteType.Bar;
+
+                default:
+                    throw new Exception($"Tried to convert a KeyType value not implemented into the mapper: {keyType}");
+            }
+        }
+    }
+}
