82ba31917508fe28390eec9e4c269a5ab15f973c
jackie <photonicdog@gmail.com>
2021-09-01 15:20:39 +0100

    feat: Add constructor for InputCommand

--- Assets/Scripts/Song/Types/InputCommand.cs
+++ Assets/Scripts/Song/Types/InputCommand.cs
@@ -1,9 +1,16 @@
 using Assets.Scripts.Song.Enums;
+
 namespace Song.Types {
     public class InputCommand {
         public double Time;
         public KeyType Key;
         public PressType PressType;
 
+        public InputCommand(double time, KeyType key, PressType pressType)
+        {
+            Time = time;
+            Key = key;
+            PressType = pressType;
+        }
     }
 }
